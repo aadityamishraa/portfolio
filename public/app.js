4edc6edd9c47abaf662ac28cb06c1de668ac3bb5
aadityamishraa <aadityamishra216@gmail.com>
2024-01-16 21:59:24 +0530

    added method for hamburger menu

---
+++ public/app.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+
+const navLink = document.querySelectorAll("nav-link");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("fa-bars");
+  hamburger.classList.toggle("fa-xmark");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.toggle("fa-bars");
+    hamburger.classList.toggle("fa-xmark");
+  });
+});
