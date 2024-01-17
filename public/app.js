040ce6d568f420101bb3ac65a84e183538bcbdca
aadityamishraa <aadityamishra216@gmail.com>
2024-01-17 18:51:20 +0530

    fetching skills from db

--- public/app.js
+++ public/app.js
@@ -3,6 +3,10 @@ const menu = document.querySelector("#menu");
 
 const navLink = document.querySelectorAll("nav-link");
 
+const skillsTitle = document.getElementsByClassName("title");
+const skillsContainers = document.getElementsByClassName("skills");
+const skillsContainers_2= document.getElementsByClassName("skills-2");
+
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   hamburger.classList.toggle("fa-bars");
@@ -16,4 +20,49 @@ navLink.forEach((link) => {
   });
 });
 
+const skillURL =
+  "https://raw.githubusercontent.com/aadityamishraa/portfolio/master/data/skills.json";
+fetch(skillURL)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < skillsTitle.length; i++) {
+      skillsTitle[i].textContent = data[i].title;
+    }
+    for (let i = 0; i < skillsContainers.length; i++) {
+      const currentContainer = skillsContainers[i];
 
+      const currentDataItem = data[i];
+
+      const ulElement = currentContainer.querySelector("ul");
+
+      if (ulElement) {
+        currentDataItem.items.forEach((skill, index) => {
+          const liElement = document.createElement("li");
+          liElement.classList.add("font-play");
+          liElement.textContent = skill;
+          if (index % 2 == 0) {
+            ulElement.appendChild(liElement);
+          }
+        });
+      }
+    }
+    for (let i = 0; i < skillsContainers_2.length; i++) {
+      const currentContainer = skillsContainers_2[i];
+
+      const currentDataItem = data[i];
+
+      const ulElement = currentContainer.querySelector("ul");
+
+      if (ulElement) {
+        currentDataItem.items.forEach((skill, index) => {
+          const liElement = document.createElement("li");
+          liElement.classList.add("font-play");
+          liElement.textContent = skill;
+          if (index % 2 != 0) {
+            ulElement.appendChild(liElement);
+          }
+        });
+      }
+    }
+  })
+  .catch((err) => console.log(err));
